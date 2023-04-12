4b6e548827043485107255771207e4f000f5b9e6
KURAMOCHI Kenta <soratobi93@gmail.com>
2023-04-12 23:43:53 +0900

    tutorial: 4.1

---
+++ control_flow.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import typing
+
+def main() -> None:
+    # if statements
+    x = int(input("Please enter an integer: ")
+    if x < 0:
+        x = 0
+        print('Negative changed to zero')
+    elif x == 0:
+        print('Zero')
+    elif x == 1:
+        print('Single')
+    else:
+        print('More')
+
+if __name__ == "__main__":
+    main()
