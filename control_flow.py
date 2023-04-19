9435249117ba914eb589746cac8c56f5c55b45d9
KURAMOCHI Kenta <soratobi93@gmail.com>
2023-04-19 23:43:58 +0900

    tutorial: 4.2

--- control_flow.py
+++ control_flow.py
@@ -14,5 +14,35 @@ def main() -> None:
     else:
         print('More')
 
+    # for statement
+    # Measure some strings:
+    words = ['cat', 'window', 'defenestrate']
+    for w in words:
+        print(w, len(w))
+
+    # Modify a collection while iterating over
+    # that same collection
+    # Create a simple collection
+    users = {'Hans': 'active',
+             'Éléonore': 'inactive',
+             '景太郎': 'active'}
+
+    # Strategy: Iterate over a copy
+    for user, status in users.copy().items():
+        if status == 'inactive':
+            del users[user]
+    print(users)
+
+    users = {'Hans': 'active',
+             'Éléonore': 'inactive',
+             '景太郎': 'active'}
+
+    # Strategy: Create a new collection
+    active_users = {}
+    for user, status in users.items():
+        if status == 'active':
+            active_users[user] = status
+    print(active_users)
+
 if __name__ == "__main__":
     main()
