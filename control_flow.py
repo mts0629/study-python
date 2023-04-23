cd36a52ed702b41e4696fca43e6e8c0ca9504252
mts0629 <soratobi93@gmail.com>
2023-04-23 12:07:33 +0900

    tutorial: fix parentheses

--- control_flow.py
+++ control_flow.py
@@ -3,7 +3,7 @@ import typing
 
 def main() -> None:
     # if statements
-    x = int(input("Please enter an integer: ")
+    x = int(input("Please enter an integer: "))
     if x < 0:
         x = 0
         print('Negative changed to zero')
